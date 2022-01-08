4c6b6bb4c3a3f625ed93c5b850e9d09af2652f81
DmitryGK <gulyakevichdmitry@gmail.com>
2022-01-08 19:50:48 +0300

    Add second list

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import './App.css';
+import {ToDolist} from "./ToDolist";
 
 function App() {
+
+    const task1 = [
+        {id:1, title: 'HTML&CSS', isDone: true},
+        {id:2, title: 'JS', isDone: true},
+        {id:3, title: 'ReactJS', isDone: false}
+    ]
+    const task2 = [
+        {id:1, title: 'Hello world', isDone: true},
+        {id:2, title: 'I am happy', isDone: true},
+        {id:3, title: 'Yo', isDone: true}
+    ]
+
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <ToDolist title='What to learn' tasks={task1}/>
+            <ToDolist title='Songs' tasks={task2}/>
         </div>
     );
 }
